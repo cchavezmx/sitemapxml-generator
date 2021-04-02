8ce6268125877ea6d86b8b1366b1ddebb93e6344
Carlos A. Chávez Rosario <66329126+cchavezmx@users.noreply.github.com>
2021-04-02 01:19:09 -0600

    primer commit y xml generado para itamx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react'
+import { renderToStaticMarkup } from 'react-dom/server'
+import axios from 'axios'
+import { render } from 'html-formatter'
 import './App.css';
 
 function App() {
+
+  const [ links, setLinks ] = useState(undefined)
+  
+  let object = []
+
+  // hacemos la consulta a la base de datos
+
+  const getDataArray = async() => {
+    await axios.get('http://localhost:3000/api/v1/catalog/products')
+    .then(res => setLinks(res.data.message))    
+  }
+
+  useEffect(() => {
+    getDataArray()
+  },[])
+
+  // cuando se cree el array metemos el contenido en el objecto
+
+  if(Array.isArray(links)){
+    links.forEach(prod => object.push(
+      { 
+        loc: `https://itamx.com/#/detalle/${prod._id}/${prod.title.replace(/[^a-zA-Z 0-9]+/g,'').trim().split(" ").join("-").toLowerCase()}`, 
+        changefreq: "weekly"   
+      }
+      ))
+  }
+
+  // un Url que esta chido
+
+  const Url = (props) => {
+    return(
+      <url>
+        {
+          Object.keys(props).map(Component => (
+            <Component>{props[Component]}</Component>
+          ))
+        }
+      </url>
+    )
+  }
+
+  // creamos el documento
+
+  const content = `
+  
+    ${render(
+      renderToStaticMarkup(
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+            {
+              object.length > 0 && object.map((link) => 
+                <Url {...link} />
+              )
+            }
+        </urlset>
+      )
+    ).replace(/\t/g, "  ")}
+  `
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <pre>{content}</pre>
   );
+
 }
 
 export default App;
